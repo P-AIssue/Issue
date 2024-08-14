836ddbb4b8564810ab75695a985908b536d5c436
khyu2 <66515313+Krockseed@users.noreply.github.com>
2024-08-14 18:46:23 +0900

    inital commit

---
+++ src/main/java/proj/AIssue/AIssueApplication.java
@@ -0,0 +1,13 @@
+package proj.AIssue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AIssueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AIssueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/proj/AIssue/AIssueApplicationTests.java
@@ -0,0 +1,13 @@
+package proj.AIssue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AIssueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
