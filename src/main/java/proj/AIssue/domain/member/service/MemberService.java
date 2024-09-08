1c9a38fd01f0db70d707f734c6d793d8a91b3d39
sonsumin <sonsumin00@gmail.com>
2024-09-08 19:15:57 +0900

    [#2]Feat: 멤버 api 서비스 로직 간단 구현

--- src/main/java/proj/AIssue/domain/member/service/MemberService.java
+++ src/main/java/proj/AIssue/domain/member/service/MemberService.java
@@ -1,14 +1,22 @@
 package proj.AIssue.domain.member.service;
 
+import proj.AIssue.domain.member.dto.MemberInfoDTO;
+import proj.AIssue.domain.member.dto.MemberUpdateDTO;
 import proj.AIssue.domain.member.entity.Member;
 
 import java.util.List;
 
 public interface MemberService {
 
-    void register(Member member);
-    void update(Member member);
-    void delete(Member member);
+    // 회원 정보 수정
+    void update(MemberUpdateDTO memberUpdateDTO, Long memberId) throws Exception;
+
+    //회원탈퇴
+    void withdraw(String password) throws Exception;
+
+    //해당 회원의 정보
+    MemberInfoDTO getInfo(Long id) throws Exception;
+
     List<Member> getMembers();
     Member getOne(Long id);
 }
--- src/main/java/proj/AIssue/domain/member/service/impl/MemberServiceImpl.java
+++ src/main/java/proj/AIssue/domain/member/service/impl/MemberServiceImpl.java
@@ -3,9 +3,55 @@ package proj.AIssue.domain.member.service.impl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import proj.AIssue.domain.member.dto.MemberInfoDTO;
+import proj.AIssue.domain.member.dto.MemberUpdateDTO;
+import proj.AIssue.domain.member.entity.Member;
+import proj.AIssue.domain.member.repository.MemberRepository;
+import proj.AIssue.domain.member.service.MemberService;
+
+import java.util.List;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class MemberServiceImpl {
+public class MemberServiceImpl implements MemberService {
+
+    private final MemberRepository memberRepository;
+
+
+    @Override
+    public void update(MemberUpdateDTO memberUpdateDTO, Long memberId) throws Exception {
+
+        Member member = memberRepository.findById(memberId).orElseThrow(
+                () -> new Exception("회원이 존재하지 않습니다."));
+
+        memberUpdateDTO.username().ifPresent(member::updateUserName);
+        memberUpdateDTO.email().ifPresent(member::updateEmail);
+
+        //필요한 필드 추가 가능
+    }
+
+    @Override
+    public MemberInfoDTO getInfo(Long id) throws Exception {
+
+        Member findMember = memberRepository.findById(id).orElseThrow(
+                () -> new Exception("회원이 존재하지 않습니다."));
+
+        return new MemberInfoDTO(findMember);
+    }
+
+
+    @Override
+    public List<Member> getMembers() {
+        return List.of();
+    }
+
+    @Override
+    public Member getOne(Long id) {
+        return null;
+    }
+
+    @Override
+    public void withdraw(String password) throws Exception {
+    }
 }
